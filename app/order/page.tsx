af69d5c2d8bbecdaff155efc3f3bb4478f62b4af
Preston Button <prestonbutton99@gmail.com>
2023-11-05 13:03:59 -0500

    add daisyUI

--- app/order/page.tsx
+++ app/order/page.tsx
@@ -2,7 +2,18 @@ import React from 'react'
 
 const order = () => {
   return (
-    <div>order</div>
+    <div>
+      <div>
+        <h1>Order Here</h1>
+        <div>
+          <h2>Main Dish</h2>
+          <label>
+            <input type='checkbox' />
+            Beef Burger
+          </label>
+        </div>
+      </div>
+    </div>
   )
 }
 
