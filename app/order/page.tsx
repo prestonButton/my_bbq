4dc65444558f8e237c618bbf3b7876cc821aa0e4
Preston Button <prestonbutton99@gmail.com>
2023-11-05 19:45:33 -0500

    update order page

--- app/order/page.tsx
+++ app/order/page.tsx
@@ -1,20 +1,55 @@
-import React from 'react'
 
-const order = () => {
+import React from 'react';
+
+interface CheckboxProps {
+  label: string;
+}
+
+const Checkbox: React.FC<CheckboxProps> = ({ label }) => {
   return (
-    <div>
+    <div className="form-control">
+      <label className="label cursor-pointer flex items-left">
+        <input type="checkbox" className="checkbox checkbox-sm" />
+        <span className="label-text text-white self-left">{label}</span>
+      </label>
+    </div>
+  );
+};
+
+const Order = () => {
+  return (
+    <div className='flex items-left justify-left'>
       <div>
         <h1>Order Here</h1>
         <div>
           <h2>Main Dish</h2>
-          <label>
-            <input type='checkbox' />
-            Beef Burger
-          </label>
+          <Checkbox label='Beef Burger' />         
+          <Checkbox label="Veggie Burger" />
+          <Checkbox label='Turkey Burger' />         
+        </div>
+        <div>
+          <h2>Customize</h2>
+          <Checkbox label='Cheese' />
+          <Checkbox label='Bacon' />
+          <Checkbox label='Tomato' />
+          <Checkbox label='Lettuce' />
+          <Checkbox label='Onion' />
+          <Checkbox label='Mushrooms' />
+          <Checkbox label='Pickles' />
+        </div>
+        <div>
+          <h2>Sides</h2>
+          <Checkbox label='Ketchup' />
+          <Checkbox label='BBQ' />
+          <Checkbox label='Mayonaise' />
+          <Checkbox label='Relish' />
+          <Checkbox label='Mustard' />
+          <Checkbox label='Honey Mustard' />
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default order
+export default Order;
+
